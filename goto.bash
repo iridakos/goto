8aa08c36abfb27552b1574df33c093a54e77e6cd
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2018-03-06 22:30:05 +0200

    Shorten message.

--- goto.bash
+++ goto.bash
@@ -102,7 +102,7 @@ function _goto_register_alias()
   fi
 
   if ! [[ $1 =~ ^[[:alnum:]]+[a-zA-Z0-9_-]*$ ]]; then
-    _goto_error "invalid alias - can start with alphanumeric followed by alphanumeric, dashes or hyphens"
+    _goto_error "invalid alias - can start with letters or digits followed by letters, digits, hyphens or underscores"
     return
   fi
 
